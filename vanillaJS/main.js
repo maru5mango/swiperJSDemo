ac9ffdab1dcfc3cf5de418907ca01cad3a2b2db1
afternine9 <afternine9@gmail.com>
2022-06-14 23:21:16 +0900

    [ADD]vanillaJS

---
+++ vanillaJS/main.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 10,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
